558b00d8355e535677fa762a3594b54ac25c62d9
Chris <c.odegard@gmail.com>
2016-11-21 17:09:01 -0500

    hello world

--- building-web-applications/app.js
+++ building-web-applications/app.js
@@ -4,6 +4,9 @@ var app = express();
 
 var port = 5000;
 
+app.get('/', function(req, res){
+    res.send('Hello, world');
+});
 app.listen(port, function(err){
     console.log('running server on port ' + port);
 });
