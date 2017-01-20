a1cf94e04dc0c4935a0da77e4006b4e637b3e3d4
Chris Odegard <c.odegard@gmail.com>
2017-01-19 21:39:42 -0500

    configured for MongoDB on el oso

--- building-web-applications/src/routes/authorRoutes.js
+++ building-web-applications/src/routes/authorRoutes.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var authorRouter = express.Router();
-var sql = require('mssql');
+//var sql = require('mssql');
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var router = function (nav) {
     authorRouter.route('/')
         .get(function (req, res) {
-            var request = new sql.Request();
+
+            // MongoDB config
+            var url = 'mongodb://el-oso:27017/LibraryApp';
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('books');
+                collection.find().toArray(
+                    function(err, results) {
+                        res.render('authorListView', {
+                            title: 'Authors',
+                            nav: nav,
+                            authors: results
+                        });
+                        db.close();
+                    }
+                );
+            });
+
+            // SQL config
+            /* var request = new sql.Request();
             request.query('select id,Author from books', 
                 function(err, recordset) {
                     console.log(err || recordset);
@@ -15,12 +35,35 @@ var router = function (nav) {
                         authors: recordset
                     });
                 }
-            ); // end query
+            ); // end query */
             
         });
     authorRouter.route('/:id')
         .get(function (req, res) {
-            var id = req.params.id;
+
+             // MongoDB config
+            var id = new objectId(req.params.id);
+            var url = 'mongodb://el-oso:27017/LibraryApp';
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('books');
+                collection.findOne({_id: id},
+                    function(err, result) {
+                        if (result.length === 0) {
+                            res.status(404).send('Not Found');
+                        } else {
+                            console.log(result);
+                            res.render('authorView', {
+                                title: 'Author',
+                                nav: nav,
+                                author: result
+                                });
+                        }
+                        db.close();
+                    }
+                );
+            });
+            // SQL Config
+           /*var id = req.params.id;
             var request = new sql.Request();
             request.query('select id,Author from books where id = ' + id, 
                 function(err, recordset) {
@@ -31,7 +74,7 @@ var router = function (nav) {
                         author: recordset[0]
                     });
                 }
-            ); // end query
+            ); // end query */
             
         });
     return authorRouter;
