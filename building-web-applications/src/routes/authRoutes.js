8a7069b8d39c6b4b86271182202fd734adea0da3
Chris Odegard <c.odegard@gmail.com>
2017-01-23 19:45:24 -0500

    configured to insert new user object

--- building-web-applications/src/routes/authRoutes.js
+++ building-web-applications/src/routes/authRoutes.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var url = require('../config/db');
 
 module.exports = function () {
     authRouter.route('/signUp')
         .post(function (req, res) {
             console.log(req.body);
-            req.login(req.body, function () {
-                res.redirect('/auth/profile');
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                var user = {
+                    username: req.body.userName,
+                    password: req.body.password
+                };
+                collection.insert(user, function (err, results) {
+                    req.login(results, function () {
+                        res.redirect('/auth/profile');
+                    });
+                });
             });
+
         }); // end .post()
 
     authRouter.route('/profile')
