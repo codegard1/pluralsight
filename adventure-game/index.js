4a0bfe61d58f754eba67b0b52e70a74938647a55
Chris Odegard <c.odegard@gmail.com>
2017-01-28 13:20:47 -0500

    new

---
+++ adventure-game/index.js
@@ -0,0 +1,12 @@
+// Check if the user is ready to play!
+
+confirm('I am ready to play!');
+
+var age = prompt('What\'s your age?');
+if (age < 13) {
+    console.log('You are not old enough to play.');
+} else {
+    console.log('Let\'s do this thing!');
+}
+
+console.log('You are at a Justin Bieber concert, and you hear this lyric \'Lace my shoes off, start racing.\'');
